6d55776d9b1218e215bbd25d58612cf8d5db741c
TeHe4K <hryblackwii@gmail.com>
2026-02-11 23:14:31 +0500

    Реализованные классы.  Коневский

---
+++ Classes/Format.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR18.Classes
+{
+    public class Format
+    {
+        public int id {  get; set; }
+        public string format { get; set; }
+        public string descrition { get; set; }
+
+        public Format(int _id, string _format, string descrition)
+        {
+            this.id = _id;
+            this.format = _format;
+            this.descrition = descrition;
+        }
+
+        public static List<Format> AllFormats()
+        {
+            List<Format> allFormat = new List<Format>();
+            allFormat.Add(new Format(1, "A4", ""));
+            allFormat.Add(new Format(2, "A3", ""));
+            allFormat.Add(new Format(3, "A2", ""));
+            allFormat.Add(new Format(4, "A1", ""));
+
+            return allFormat;
+        }
+    }
+}
---
+++ Classes/TypeOperation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR18.Classes
+{
+    public class TypeOperation
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string descrition { get; set; }
+
+        public TypeOperation(int _id, string _name, string descrition)
+        {
+            this.id = _id;
+            this.name = _name;
+            this.descrition = descrition;
+        }
+
+        public static List<TypeOperation> AllTypeOperation()
+        {
+            List<TypeOperation> allTypeOperation = new List<TypeOperation>();
+            allTypeOperation.Add(new TypeOperation(1, "Печать", ""));
+            allTypeOperation.Add(new TypeOperation(2, "Копия", ""));
+            allTypeOperation.Add(new TypeOperation(3, "Сканирование", ""));
+            allTypeOperation.Add(new TypeOperation(4, "Ризограф", ""));
+
+            return allTypeOperation;
+        }
+    }
+}
